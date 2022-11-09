eee0fee18382e58d1f15c774df633e40e96686de
Jack742 <9195368+Jack742@users.noreply.github.com>
2022-11-09 17:54:57 +0000

    Fixed some relative imports, pushing simple MLP and test

--- BTFR/Tests/test_models.py
+++ BTFR/Tests/test_models.py
@@ -1,11 +1,10 @@
 import pytest
 import torch
 
-from ..Models.MLP import MCDropout_MLP
-
+from Models.MLP import MCDropout_MLP
 def test_MCDropout_MLP_output_shape():
     inp = 10
     out = 2
-    x = torch.linspace(0,inp, dtype=torch.float)
+    x = torch.arange(0,inp, dtype=torch.float)
     model = MCDropout_MLP(inp,out,(inp, inp//2))
     assert(len(model(x))==out)
